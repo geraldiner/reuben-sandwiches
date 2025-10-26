fc832d7e878f2f697b049af081b82838076b77f1
Geraldine Ragsac <hello@geraldiner.com>
2025-10-26 18:54:10 -0400

    Add some tests

---
+++ src/test/ItemCard.test.js
@@ -0,0 +1,23 @@
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { expect, test } from 'vitest';
+import ItemCard from '../components/ItemCard.astro';
+
+test('ItemCard with slots', async () => {
+  const container = await AstroContainer.create();
+  const result = await container.renderToString(ItemCard, {
+    props: {
+        restaurant: "Testaurant",
+        website: "https://testaurant.example.com",
+        item: "Test Item",
+        sides: "fries",
+        stars: 2,
+        price: 9.50
+    }
+  });
+  expect(result).toContain('Testaurant').toContain('href="https://testaurant.example.com"');
+  expect(result).toContain('Test Item');
+  expect(result).toContain('🥪'.repeat(2));
+  expect(result).toContain('✖️'.repeat(3));
+  expect(result).toContain('$');
+  expect(result).toContain('$9.50');
+});
